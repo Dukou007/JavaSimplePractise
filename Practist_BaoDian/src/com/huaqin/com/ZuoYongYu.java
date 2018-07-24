91046c691db7dd25a5adffb651692bdb33913661
dukou007 <dukou_caiqin@163.com>
2018-07-24 11:56:03 +0800

    Java缓存机制      2018/7/24      13:40

---
+++ Practist_BaoDian/src/com/huaqin/com/ZuoYongYu.java
@@ -0,0 +1,12 @@
+package com.huaqin.com;
+
+public class ZuoYongYu {
+    public static void main(String[] args) {
+        int j = 0;
+            for (int i=0;i<100;i++){
+
+                j=j++;
+            }
+        System.out.println(j);
+    }
+}
